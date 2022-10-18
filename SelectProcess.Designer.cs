9bb7e9bc8bb69cd5b79d99e79cd37ca78e8f24f1
Jerry <gao1021229073@163.com>
2022-10-18 23:11:53 +0800

    reset trace window

--- SelectProcess.Designer.cs
+++ SelectProcess.Designer.cs
@@ -28,34 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lvProcDetail = new System.Windows.Forms.ListView();
             this.btnTrace = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbExePath = new System.Windows.Forms.TextBox();
             this.btnSelProcess = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lvProcDetail
-            // 
-            this.lvProcDetail.FullRowSelect = true;
-            this.lvProcDetail.GridLines = true;
-            this.lvProcDetail.Location = new System.Drawing.Point(12, 72);
-            this.lvProcDetail.MultiSelect = false;
-            this.lvProcDetail.Name = "lvProcDetail";
-            this.lvProcDetail.Size = new System.Drawing.Size(949, 1007);
-            this.lvProcDetail.TabIndex = 0;
-            this.lvProcDetail.UseCompatibleStateImageBehavior = false;
-            this.lvProcDetail.View = System.Windows.Forms.View.Details;
             // 
             // btnTrace
             // 
             this.btnTrace.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnTrace.Location = new System.Drawing.Point(781, 1097);
+            this.btnTrace.Location = new System.Drawing.Point(542, 164);
             this.btnTrace.Name = "btnTrace";
             this.btnTrace.Size = new System.Drawing.Size(180, 72);
-            this.btnTrace.TabIndex = 1;
+            this.btnTrace.TabIndex = 2;
             this.btnTrace.Text = "Trace!";
             this.btnTrace.UseVisualStyleBackColor = true;
             this.btnTrace.Click += new System.EventHandler(this.btnTrace_Click);
@@ -65,30 +53,30 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(562, 62);
+            this.label1.Size = new System.Drawing.Size(730, 62);
             this.label1.TabIndex = 2;
             this.label1.Text = "When process starts, auto saver will start as well\r\n..process exits, auto saver w" +
-    "ill stop saving.";
+    "ill save last one and stop auto-save";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 1085);
+            this.label2.Location = new System.Drawing.Point(12, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(441, 31);
+            this.label2.Size = new System.Drawing.Size(409, 31);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Or choose program waiting to launch";
+            this.label2.Text = "Choose program waiting to launch";
             // 
-            // textBox1
+            // tbExePath
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 1119);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(628, 38);
-            this.textBox1.TabIndex = 4;
+            this.tbExePath.Location = new System.Drawing.Point(12, 117);
+            this.tbExePath.Name = "tbExePath";
+            this.tbExePath.Size = new System.Drawing.Size(828, 38);
+            this.tbExePath.TabIndex = 4;
             // 
             // btnSelProcess
             // 
-            this.btnSelProcess.Location = new System.Drawing.Point(646, 1114);
+            this.btnSelProcess.Location = new System.Drawing.Point(846, 112);
             this.btnSelProcess.Name = "btnSelProcess";
             this.btnSelProcess.Size = new System.Drawing.Size(62, 46);
             this.btnSelProcess.TabIndex = 5;
@@ -100,17 +88,30 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnCancel.Location = new System.Drawing.Point(728, 164);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(180, 72);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
             // SelectProcess
             // 
+            this.AcceptButton = this.btnTrace;
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(973, 1181);
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(920, 248);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSelProcess);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbExePath);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnTrace);
-            this.Controls.Add(this.lvProcDetail);
             this.Name = "SelectProcess";
             this.Text = "Select Process to Trace";
             this.Load += new System.EventHandler(this.SelectProcess_Load);
@@ -120,13 +121,12 @@
         }
 
         #endregion
-
-        private ListView lvProcDetail;
         private Button btnTrace;
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
+        private TextBox tbExePath;
         private Button btnSelProcess;
         private OpenFileDialog openFileDialog1;
+        private Button btnCancel;
     }
 }
--- SelectProcess.cs
+++ SelectProcess.cs
@@ -28,42 +28,7 @@ namespace SaveMyGame
         } 
         private void SelectProcess_Load(object sender, EventArgs e)
         {
-            lvProcDetail.Items.Clear();
-            lvProcDetail.Columns.Add("PID", (int)(lvProcDetail.Width*0.1));
-            lvProcDetail.Columns.Add("Name", (int)(lvProcDetail.Width*0.35));
-            lvProcDetail.Columns.Add("Running time", (int)(lvProcDetail.Width*0.1));
-            lvProcDetail.Columns.Add("Launch time", (int)(lvProcDetail.Width * 0.35));
-            
-            foreach (var proc in System.Diagnostics.Process.GetProcesses())
-            {
-                string ProcessName = "";
-                long pid = 0;
-                string StartTime = "";
-                double runningtime = 0;
-                try
-                {
-                    ProcessName = proc.ProcessName;
-                    pid = proc.Id;
-                    runningtime = (double)((proc.TotalProcessorTime.TotalMilliseconds / 1000.0));
-                    StartTime = proc.StartTime.ToString("yyyy/MM/dd hh:mm:ss.ffff");
-                }
-                catch(Exception ex)
-                {
-                    //Debug.WriteLine(ex.Message);
-                }
-                finally
-                {
 
-                    ListViewItem lvitem = new ListViewItem();
-                    lvitem.Text = pid.ToString();
-                    lvitem.SubItems.Add(ProcessName);
-                    lvitem.SubItems.Add(runningtime.ToString("f2") + "ms");
-                    lvitem.SubItems.Add(StartTime);
-                    lvProcDetail.Items.Add(lvitem);
-                }
-            }
-
-            lvProcDetail.Sort();
         }
 
         private void btnSelProcess_Click(object sender, EventArgs e)
@@ -76,7 +41,7 @@ namespace SaveMyGame
             {
                 if (System.IO.File.Exists(openFileDialog.FileName))
                 {
-                    textBox1.Text = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                    tbExePath.Text = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
                     
                 }
             }
@@ -85,32 +50,26 @@ namespace SaveMyGame
 
         private void btnTrace_Click(object sender, EventArgs e)
         {
-            Process proc = new Process();
-            try
-            {
-                proc = Process.GetProcessById(int.Parse(lvProcDetail.SelectedItems[0].Text));
-            }
-            catch (Win32Exception ex)
-            {
-                MessageBox.Show("Permission denined!\nThis Process cannot be monitored.\n-----\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                lvProcDetail.SelectedItems.Clear();
-                lvProcDetail.Refresh();
-            }
-            finally
-            {
-                if(proc != null)
-                {
-                    textBox1.Text = proc.ProcessName;
-                }
-            }
-            if(!string.IsNullOrEmpty(textBox1.Text))
+            if(!string.IsNullOrEmpty(tbExePath.Text))
             {
                 MessageBox.Show("Choose at least one process to trace!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
             else
             {
-                SelectedProcessExecutable = textBox1.Text;
+                if(System.IO.File.Exists(tbExePath.Text))
+                {
+                    SelectedProcessExecutable = tbExePath.Text;
+                }
+                else
+                {
+                    MessageBox.Show("File not exists!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
             }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
